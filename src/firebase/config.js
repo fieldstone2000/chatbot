2a15c77bad4456a8121c7f511f252f96dc599a88
Satoshi Ishihara <Satoshi Ishihara>
2019-02-17 09:14:31 +0900

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDaUE29xPCBEZdOBJOBO21c1P5CttTNL7U",
+    authDomain: "simple-chat-1e6af.firebaseapp.com",
+    databaseURL: "https://simple-chat-1e6af.firebaseio.com",
+    projectId: "simple-chat-1e6af",
+    storageBucket: "simple-chat-1e6af.appspot.com",
+    messagingSenderId: "690025630527"
+}
